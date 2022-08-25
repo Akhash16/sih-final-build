fa61d8360919db47354232a9eb2439eac2a562af
Akhash S <akhash.xs@gmail.com>
2022-08-25 23:30:04 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImgShuffle from './components/ImgPuzzle';
+
+function App() {
+  return (
+    <div className="App">
+      <ImgShuffle/>
+    </div>
+  );
+}
+
+export default App;
