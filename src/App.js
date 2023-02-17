ea715cea2f0525c5cdd72a8fa2075fa9ac844194
Daadreyaa D <daadreyaa@gmail.com>
2023-02-17 10:49:55 +0530

    Fresh Start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 
 import './App.css';
-import ImgShuffle from './components/ImgPuzzle';
+import ImgShuffle from './components/Puzzles/ImgPuzzle';
 import './styles.css'
-import PatternPuzzle from './components/PatternPuzzle';
+import PatternPuzzle from './components/Puzzles/PatternPuzzle';
 
-import LanguageSelect from './components/LanguagePuzzle';
-import PolygonPuzzle from './components/PolygonPuzzle';
+import LanguageSelect from './components/Puzzles/LanguagePuzzle';
+import PolygonPuzzle from './components/Puzzles/PolygonPuzzle';
+import ConcentricCircle from './components/Puzzles/ConcentricCircle';
+import Puzzles from './components/Puzzles/Puzzles';
+import SignUp from './components/SignUp/signup';
 
 
 function App() {
   return (
     <div className="App">
       <>
-      {/* <Main/> */}
-      {/* <ImgShuffle/> */}
-      {/* <PatternPuzzle/> */}
-       
-      <LanguageSelect/>
-      {/* <PolygonPuzzle/> */}
+        {/* <Main /> */}
+        {/* <ImgShuffle /> */}
+        {/* <PatternPuzzle /> */}
+        <LanguageSelect />
+        {/* <PolygonPuzzle /> */}
+        {/* <ConcentricCircle /> */}
+        {/* <ImgShuffle /> */}
+        {/* <Puzzles /> */}
+        {/* <SignUp /> */}
       </>
-      
-      
+
+
     </div>
   );
 }
